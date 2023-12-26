c6bc0a8ffb8918af9972c9e7bc2afe01c2ccd985
Dorian Davy <dorian.nt89@outlook.com>
2023-12-26 09:27:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+
+//Mostar u Ocultar el Menu
+function MostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
+
+//Animaciones de Skills
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("java");
+        habilidades[3].classList.add("sqldeveloper");
+        habilidades[4].classList.add("csharp");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajos");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
